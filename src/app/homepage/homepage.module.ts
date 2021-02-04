c79cd5b13cb6f56fea9079fc19072e6a6c6556bb
FranglinSeed <franglin38@gmail.com>
2021-02-04 18:44:49 +0300

    feat(homepage): add login mat dialog

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
@@ -7,15 +8,20 @@ import { LogoComponent } from './logo/logo.component';
 import { HfooterComponent } from './hfooter/hfooter.component';
 import { IconModule } from '../ui-kit/icon/icon.module';
 import { PipesModule } from '../ui-kit/pipes/pipes.module';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [HomepageComponent, LogoComponent, HfooterComponent],
+  declarations: [HomepageComponent, LogoComponent, HfooterComponent, LoginComponent],
   imports: [
     CommonModule,
+    MatDialogModule,
     HomepageRoutingModule,
     IconModule,
     PipesModule,
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class HomepageModule { }
--- src/app/homepage/logo/logo.component.ts
+++ src/app/homepage/logo/logo.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'sdate-logo',
@@ -8,13 +11,28 @@ import { Router } from '@angular/router';
 })
 export class LogoComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private dialog: MatDialog,
+              private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  onLoginClicked(): void {
-    this.router.navigate(['/home']);
+  onLoginClicked($event): void {
+    $event.preventDefault();
+    const dialogRef = this.dialog.open(LoginComponent, {
+      panelClass: 'full-panel',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && result.length) {
+        if ( result.toString() === 'login success' ) {
+          this.router.navigate(['home']);
+        } else {
+          alert(result.toString());
+        }
+      }
+    });
   }
 
 }
